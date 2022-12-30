80529cf9c2f48df9cea7528059bfef64e9c7faf7
Eddie Yen <36756732+rgmmmt4r@users.noreply.github.com>
2022-12-30 19:22:12 +0800

    Delete FZNAlarm directory

--- FZNAlarm/app/src/main/java/com/example/fznalarm/util/RandomIntUtil.kt
+++
@@ -1,9 +0,0 @@
-package com.example.fznalarm.util
-
-import com.example.fznalarm.MainActivity
-import java.util.concurrent.atomic.AtomicInteger
-
-object RandomIntUtil {
-    private val seed = AtomicInteger()
-    fun getRandomInt() = seed.getAndIncrement() +  System.currentTimeMillis().toInt()
-}
