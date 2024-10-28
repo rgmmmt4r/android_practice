38c832132fd3dfc0030313f45e6abd96077d44fe
Eddie Yen <36756732+rgmmmt4r@users.noreply.github.com>
2024-10-28 16:54:38 +0800

    Service complete

--- Service/app/src/main/java/com/example/service/SecActivity.kt
+++ Service/app/src/main/java/com/example/service/SecActivity.kt
@@ -1,32 +1,20 @@
 package com.example.service
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-
+class SecActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sec)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        findViewById<Button>(R.id.btnStart).setOnClickListener(){
-            startService(Intent(this,MyService::class.java))
-            Toast.makeText(
-                this,"啟動 Service",Toast.LENGTH_SHORT
-            ).show()
-            finish()
         }
     }
 }
