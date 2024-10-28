91cfea17569550acaaac1032a72a0a58cfecd164
Eddie Yen <36756732+rgmmmt4r@users.noreply.github.com>
2024-10-28 11:16:39 +0800

    Media Recorder init

--- MediaRecorder/app/src/main/java/com/example/mediarecorder/MainActivity.kt
+++ MediaRecorder/app/src/main/java/com/example/mediarecorder/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.mediarecorder
 
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recorder: MediaRecorder
+    private lateinit var player: MediaPlayer
+    private lateinit var folder: File
+    private var fileName = ""
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(grantResults.isNotEmpty() && requestCode == 0){
+            if (grantResults[0] == PackageManager.PERMISSION_DENIED){
+                finish()
+            }else{
+                doInitialize()
+                setListener()
+            }
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +44,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val permission = android.Manifest.permission.RECORD_AUDIO
+
+        if (ActivityCompat.checkSelfPermission(this,permission)
+            !=PackageManager.PERMISSION_GRANTED
+            ){
+            ActivityCompat.requestPermissions(
+                this, arrayOf(permission),0
+            )
+        } else{
+            doInitialize()
+            setListener()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if(::recorder.isInitialized) recorder.release()
+        if(::player.isInitialized) player.release()
+    }
+
+    private fun doInitialize(){
+    }
+
+
+    private fun setListener(){
     }
 }
