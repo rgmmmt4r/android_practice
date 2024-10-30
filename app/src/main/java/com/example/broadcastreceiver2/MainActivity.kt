4cd4b22c55c0f2b70da76c052441d6f2e1b2cac3
Eddie Yen <rgmmmt4r@gmail.com>
2024-10-30 10:28:13 +0800

    dynamic broadcast receiver

--- app/src/main/java/com/example/broadcastreceiver2/MainActivity.kt
+++ app/src/main/java/com/example/broadcastreceiver2/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.broadcastreceiver2
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val receiver =
+        object : BroadcastReceiver(){
+            override fun onReceive(context: Context, intent: Intent) {
+                val action = intent.action
+                val msg = intent.extras?.getString("msg")
+                Toast.makeText(context,"螢幕開啟",Toast.LENGTH_SHORT).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +31,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val intentFilter = IntentFilter(Intent.ACTION_SCREEN_ON)
+
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            registerReceiver(receiver, intentFilter, RECEIVER_EXPORTED)
+        }else{
+            registerReceiver(receiver, intentFilter)
+        }
+//        val intent = Intent("MyBroadcast")
+//        intent.putExtra("msg","Hello, World")
+//        sendBroadcast(intent)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
+    }
+
 }
