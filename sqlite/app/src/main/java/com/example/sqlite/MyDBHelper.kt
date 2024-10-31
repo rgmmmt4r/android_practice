44d7a2c75c19b5aaaa781420757ba3d8d98e3004
Eddie Yen <36756732+rgmmmt4r@users.noreply.github.com>
2024-10-31 18:34:45 +0800

    sqlite complete

---
+++ sqlite/app/src/main/java/com/example/sqlite/MyDBHelper.kt
@@ -0,0 +1,29 @@
+package com.example.sqlite
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.os.Build.VERSION
+
+class MyDBHelper (
+    context: Context,
+    name: String = DB_Name,
+    factory: SQLiteDatabase.CursorFactory? = null,
+    version: Int = VERSION
+): SQLiteOpenHelper(context,name,factory,version){
+    companion object{
+        private  const val DB_Name = "myDatabase"
+        private  const val VERSION = 1
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL("CREATE TABLE myTable(book text PRIMARY KEY,price interger not NULL)")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("Drop TABLE IF EXISTS myTable")
+        onCreate(db)
+    }
+
+}
+
