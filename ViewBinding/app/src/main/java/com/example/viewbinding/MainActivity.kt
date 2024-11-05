3f22893a2047ef638251f345b3f70c36df2719fc
Eddie Yen <rgmmmt4r@gmail.com>
2024-11-05 17:49:56 +0800

    Data binding exercise

---
+++ ViewBinding/app/src/main/java/com/example/viewbinding/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.viewbinding
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingComponent
+import androidx.databinding.DataBindingUtil
+import com.example.viewbinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        binding.title = "我使用DataBinding"
+
+        binding.root.setOnClickListener(){
+            if(binding.title.isNullOrBlank()){
+                binding.title = "我使用DataBinding"
+            }else{
+                binding.title = null
+            }
+        }
+    }
+}
