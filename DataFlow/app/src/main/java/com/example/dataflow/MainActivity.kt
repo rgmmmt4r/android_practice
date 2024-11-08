0d9f5c5a446ae03938ad449f8d85c6e1c3458451
Eddie Yen <rgmmmt4r@gmail.com>
2024-11-08 10:58:37 +0800

    DataFlow complete

---
+++ DataFlow/app/src/main/java/com/example/dataflow/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.dataflow
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBinderMapper
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.dataflow.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        doInitialize()
+
+    }
+
+    private fun doInitialize() {
+        enableEdgeToEdge()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        binding.lifecycleOwner = this
+        binding.vm = viewModel
+    }
+}
